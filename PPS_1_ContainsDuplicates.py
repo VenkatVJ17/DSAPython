97d9d1e12a8a034ebecdc65b6109720e59c156c9
VenkatVJ17 <venkat.jalaneela@hotmail.com>
2024-11-27 04:59:28 -0600

    initial

---
+++ PPS_1_ContainsDuplicates.py
@@ -0,0 +1,35 @@
+"""
+Problem Description:
+You are given an array of integers, arr, containing n elements. Your task is to identify all the elements that appear more than once in the array. 
+If no duplicates exist, return an empty list.
+
+Input:
+
+An array of integers, arr, of size n where 1 ≤ n ≤ 10^5.
+The integers in the array can be both positive and negative and lie in the range -10^6 ≤ arr[i] ≤ 10^6.
+Output:
+
+A list of integers representing the duplicate elements in the array.
+The output list can be in any order, but each duplicate element should appear only once in the output.
+Input: arr = [1, 1, 2]
+Output: [1]
+Explanation: Only 1 appears more than once in the array.
+
+"""
+def containsDuplicates(intarr):
+    myDict = {}
+    for i in range(len(intarr)):
+        if intarr[i] in myDict:
+            myDict[intarr[i]]=myDict[intarr[i]]+1
+        else:
+            myDict[intarr[i]]=1    
+    duplicates = [key for key, value in myDict.items() if value > 1]
+    print(duplicates)
+
+def main():
+    # Sample input list
+    intarr = [1, 2, 3, 4, 5, 5,4,2]
+    containsDuplicates(intarr)
+
+if __name__=="__main__":
+    main()
